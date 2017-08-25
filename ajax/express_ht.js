c1c08c56a7ca00f89e64543ade680e87193b0306
Amey <amey@llip.life>
2017-08-25 15:13:18 -0700

    any url change in js

--- ajax/express_ht.js
+++ ajax/express_ht.js
@@ -3,7 +3,7 @@ const express =require("express");
 const app=express();
 app.listen(3614);
 app.use(express.static('public'));
-app.get('/XMLHttpRequest',(req,res)=>{
+app.get('/anyurl',(req,res)=>{
   fs.appendFile('/home/amey/garageScript/ajax/public/data.txt',`\n Name: ${req.query.username} comment: ${req.query.comment} \n `);
   res.send('done');
 });
