cc6ae08dbc13d04ec979a5b82448952797eae9b0
Amey <amey@llip.life>
2017-08-25 13:27:29 -0700

    5.ajax wo chatroom

--- chatroom/chatroom.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const fs = require("fs");
-const app = express();
-app.listen(3614);
-app.use(express.static('public'));
-app.get('/XMLHttpRequest', (req,res) =>{
-    fs.appendFile('/home/amey/garageScript/chatroom/public/data.txt',`/n name: ${req.query.Name} comment: ${req.query.Comment}`);
-    res.send("your comment have been saved");
-    });
