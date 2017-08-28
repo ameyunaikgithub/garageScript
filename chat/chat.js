ce2c823db3d2949c62142a6cd6291b0a023ac8ed
Amey <amey@llip.life>
2017-08-28 12:30:37 -0700

    chat with indent

--- chat/chat.js
+++ chat/chat.js
@@ -4,7 +4,7 @@ const app = express();
 app.listen(3614);
 app.use(express.static('public'));
 app.get('/anyurl', (req,res) =>{
-    fs.appendFile('/home/amey/garageScript/chat/public/data.txt',` name: ${req.query.Name} comment: ${req.query.Comment}\n`);
-    res.send("comment recorded and saved");
-    });
+  fs.appendFile('/home/amey/garageScript/chat/public/data.txt',` name: ${req.query.Name} comment: ${req.query.Comment}\n`);
+  res.send("comment recorded and saved");
+});
 
