1ecc10b8e92c21c4e3d2ec8b3a5a31c23eb55834
Amey <amey@llip.life>
2017-09-06 17:32:53 -0700

    latest commits

--- chat/chat.js
+++ chat/chat.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const app = express();
 app.listen(3614);
 app.use(express.static('public'));
-app.get('/anyurl', (req,res) =>{
+app.get('/newchat', (req,res) =>{
   fs.appendFile('/home/amey/garageScript/chat/public/data.txt',` name: ${req.query.Name} comment: ${req.query.Comment}\n`);
   res.send("comment recorded and saved");
 });
